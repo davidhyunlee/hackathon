26edb70a143acc34dbb4de4a9dd75ca8fd8d8bbc
Calvin <calvinliu24@gmail.com>
2015-05-19 10:46:55 -0700

    figaro install and consuming google api in controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def index
+  	url = "https://maps.googleapis.com/maps/api/js?key="
+  	key = ENV['GOOGLE_MAPS']
+  	@endpoint = url + key
   end
 end
